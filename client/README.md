62cde3b0df6a84a6b71051eaf205c863e13cd182
amcereijo <amcereijo@gmail.com>
2016-09-27 19:32:47 +0200

    add files for client side

---
+++ client/README.md
@@ -0,0 +1,2 @@
+# Business client #
+Based on [Starter for Angular + ES6 + (Webpack or JSPM)](https://github.com/AngularClass/NG6-starter)
---
+++ client/client/app/components/home/home.component.js
@@ -0,0 +1,13 @@
+import template from './home.html';
+import controller from './home.controller';
+import './home.styl';
+import 'bootstrap/dist/css/bootstrap.css';
+
+let homeComponent = {
+  restrict: 'E',
+  bindings: {},
+  template,
+  controller
+};
+
+export default homeComponent;
